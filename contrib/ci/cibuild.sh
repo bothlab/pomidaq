9306bb3bbed9c77e5fd96b85308cfdc538fb7c56
Matthias Klumpp <matthias@tenstral.net>
2019-04-18 13:49:20 +0200

    ci: Add correct version to built Debian package

--- contrib/ci/cibuild.sh
+++ contrib/ci/cibuild.sh
@@ -28,6 +28,16 @@ rm -rf build/
 #
 # Build Debian package
 #
+
+git_commit=$(git log --pretty="format:%h" -n1)
+git_current_tag=$(git describe --tags 2> /dev/null || echo "v0.1")
+git_commit_no=$(git rev-list --count HEAD)
+upstream_version=$(echo "${git_current_tag}" | sed 's/^v\(.\+\)$/\1/;s/[-]/./g')
+upstream_version="$upstream_version+git$git_commit_no"
+
 mv contrib/debian .
+dch --distribution "UNRELEASED"	--newversion="${upstream_version}" -b \
+    "New automated build from: ${upstream_version} - ${git_commit}"
+
 dpkg-buildpackage
 mv ../*.deb .
