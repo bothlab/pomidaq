42a38d657b88f39499d3f57ab3f21ec4d738ac6a
Matthias Klumpp <matthias@tenstral.net>
2020-02-03 14:12:38 +0100

    Set dark theme by default on Linux

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,64 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#ifdef Q_OS_LINUX
+#include <KSharedConfig>
+#include <KColorScheme>
+#endif
+
+#ifdef Q_OS_LINUX
+static void changeColorScheme(const QString &filename, bool darkColors = false)
+{
+    auto config = KSharedConfig::openConfig(filename);
+
+    QPalette palette = qApp->palette();
+    QPalette::ColorGroup states[3] = { QPalette::Active, QPalette::Inactive, QPalette::Disabled };
+    KColorScheme schemeTooltip(QPalette::Active, KColorScheme::Tooltip, config);
+
+    for (int i = 0; i < 3 ; ++i) {
+        QPalette::ColorGroup state = states[i];
+        KColorScheme schemeView(state,      KColorScheme::View,      config);
+        KColorScheme schemeWindow(state,    KColorScheme::Window,    config);
+        KColorScheme schemeButton(state,    KColorScheme::Button,    config);
+        KColorScheme schemeSelection(state, KColorScheme::Selection, config);
+
+        palette.setBrush(state, QPalette::WindowText,      schemeWindow.foreground());
+        palette.setBrush(state, QPalette::Window,          schemeWindow.background());
+        palette.setBrush(state, QPalette::Base,            schemeView.background());
+        palette.setBrush(state, QPalette::Text,            schemeView.foreground());
+        palette.setBrush(state, QPalette::Button,          schemeButton.background());
+        palette.setBrush(state, QPalette::ButtonText,      schemeButton.foreground());
+        palette.setBrush(state, QPalette::Highlight,       schemeSelection.background());
+        palette.setBrush(state, QPalette::HighlightedText, schemeSelection.foreground());
+        palette.setBrush(state, QPalette::ToolTipBase,     schemeTooltip.background());
+        palette.setBrush(state, QPalette::ToolTipText,     schemeTooltip.foreground());
+
+        palette.setColor(state, QPalette::Light,           schemeWindow.shade(KColorScheme::LightShade));
+        palette.setColor(state, QPalette::Midlight,        schemeWindow.shade(KColorScheme::MidlightShade));
+        palette.setColor(state, QPalette::Mid,             schemeWindow.shade(KColorScheme::MidShade));
+        palette.setColor(state, QPalette::Dark,            schemeWindow.shade(KColorScheme::DarkShade));
+        palette.setColor(state, QPalette::Shadow,          schemeWindow.shade(KColorScheme::ShadowShade));
+
+        palette.setBrush(state, QPalette::AlternateBase,   schemeView.background(KColorScheme::AlternateBackground));
+        palette.setBrush(state, QPalette::Link,            schemeView.foreground(KColorScheme::LinkText));
+        palette.setBrush(state, QPalette::LinkVisited,     schemeView.foreground(KColorScheme::VisitedText));
+    }
+
+    qApp->setProperty("KDE_COLOR_SCHEME_PATH", filename);
+    qApp->setPalette(palette);
+
+    QIcon::setThemeName(darkColors ? QStringLiteral("breeze-dark") : QStringLiteral("breeze"));
+}
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+#ifdef Q_OS_LINUX
+    changeColorScheme(QStringLiteral("/usr/share/color-schemes/BreezeDark.colors"), true);
+#endif
+
     MainWindow w;
     w.show();
 
