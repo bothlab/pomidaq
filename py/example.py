be95b96c5d6887ee8208d0bccfe60b218e2e1ff2
Matthias Klumpp <matthias@tenstral.net>
2020-08-06 23:53:25 +0200

    python: Permit usage of any Miniscope type from Python as well
    The Python API should now be on the same level as the C++ API again.

--- py/example.py
+++ py/example.py
@@ -2,34 +2,55 @@
 
 #
 # Small example on how to use the Miniscope library from Python
-# In order to make this example work, first install the Python
+# In order to make this example work, first install the "miniscope" Python
 # module with CMake.
-# Alternatively you can also place the miniscope library into a path you like
-# and then change PYTHONPATH (sys.path) to find it.
+# Alternatively you can also place the compiled "miniscope" module into a
+# path you like and then change PYTHONPATH (sys.path) so Python can find it.
 #
 
 import sys
 import cv2
-from miniscope import Miniscope, VideoCodec, VideoContainer
+from miniscope import Miniscope, ControlKind
 
-MINISCOPE_DEVICE = 'Miniscope_V4'
+MINISCOPE_DEVICE = 'Miniscope_V4'  # The device type we want to connect to
+DEVICE_ID = 0  # the video device ID of our DAQ box
+VIDEO_FILENAME = '/tmp/miniscope-test.mkv'  # name of the saved video
 
+# create new Miniscope instance
 mscope = Miniscope()
+
+# disable some debug/info messages about data transmission
+# to make the console output of this example easier to read
+mscope.set_print_extra_debug(False)
 
 # list all available miniscope types
 print('Available Miniscope harware types:')
 for dname in mscope.available_device_types:
     print(' * {}'.format(dname))
 
+print()
 print('Selecting: {}'.format(MINISCOPE_DEVICE))
-
 if not mscope.load_device_config(MINISCOPE_DEVICE):
     print('Unable to load device configuration for {}: {}'.format(MINISCOPE_DEVICE, mscope.last_error),
           file=sys.stderr)
     sys.exit(1)
 
-mscope.set_cam_id(0)  # Connect to video camera 0
+print('Available controls:')
+controls = {}
+for ctl in mscope.controls:
+    controls[ctl.id] = ctl
+    value_start = ctl.value_start
+    if ctl.kind == ControlKind.SELECTOR:
+        default_val = value_start
+        if int(value_start) < len(ctl.labels):
+            # replace the number with a human-readable string´
+            default_val = ctl.labels[int(value_start)]
+        print(' * {}: {} (default value: {})'.format(ctl.id, ctl.name, default_val))
+    else:
+        print(' * {}: {} (default value: {})'.format(ctl.id, ctl.name, value_start))
 
+print('Connecting to device with ID: {}\n'.format(DEVICE_ID))
+mscope.set_cam_id(DEVICE_ID)
 if not mscope.connect():
     print('Unable to connect to Miniscope: {}'.format(mscope.last_error), file=sys.stderr)
     sys.exit(1)
@@ -38,8 +59,18 @@ if not mscope.run():
     print('Unable to start data acquisition: {}'.format(mscope.last_error), file=sys.stderr)
     sys.exit(1)
 
+# adjust some controls
+GAIN_VAL = 1
+print('Setting gain control to {}'.format(controls['gain'].labels[GAIN_VAL]))
+mscope.set_control_value('gain', GAIN_VAL)
+
+EXCITATION_VAL = 20
+print('Setting excitation control to {}'.format(EXCITATION_VAL))
+mscope.set_control_value('led0', EXCITATION_VAL)
+
+# prepare video recording
 print('\n--------')
-mscope.video_filename = '/tmp/miniscope-test.mkv'
+mscope.video_filename = VIDEO_FILENAME
 print('Codec used for recording: {}'.format(mscope.video_codec))
 print('Container used for recording: {}'.format(mscope.video_container))
 print('Saving video in: {}'.format(mscope.video_filename))
@@ -50,7 +81,7 @@ if not mscope.start_recording(''):
     sys.exit(1)
 
 try:
-    print('Recording... Terminate with CTL+C')
+    print('Recording... Terminate with CTL+C\n')
     while mscope.is_running:
         frame = mscope.current_disp_frame
         if frame is not None:
@@ -58,6 +89,7 @@ try:
             cv2.waitKey(50)
 except KeyboardInterrupt:
     print('User terminated recording. Shutting down.')
+    print('Timestamp of last recorded frame: {}'.format(mscope.last_recorded_frame_time))
     mscope.stop()
 
 if mscope.last_error:
--- py/pyminiscope.cpp
+++ py/pyminiscope.cpp
@@ -21,6 +21,8 @@
 #include <sstream>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+#include <pybind11/chrono.h>
 #include "qstringtopy.h"
 #include "cvmatndsliceconvert.h"
 #include "miniscope.h"
@@ -28,10 +30,15 @@
 using namespace MScope;
 namespace py = pybind11;
 
+PYBIND11_MAKE_OPAQUE(std::vector<ControlDefinition>);
+PYBIND11_MAKE_OPAQUE(std::vector<double>);
+
 PYBIND11_MODULE(miniscope, m) {
     m.doc() = "Access a Miniscope through Python"; // optional module docstring
 
     NDArrayConverter::initNDArray();
+    py::bind_vector<std::vector<double>>(m, "VectorDouble");
+    py::bind_vector<std::vector<ControlDefinition>>(m, "VectorControlDefinition");
 
     py::enum_<VideoCodec>(m, "VideoCodec", py::arithmetic())
             .value("UNKNOWN", VideoCodec::Unknown)
@@ -42,20 +49,43 @@ PYBIND11_MODULE(miniscope, m) {
             .value("HEVC", VideoCodec::HEVC)
             .value("MPEG4", VideoCodec::MPEG4)
             .export_values()
-            ;
+    ;
 
     py::enum_<VideoContainer>(m, "VideoContainer", py::arithmetic())
             .value("UNKNOWN", VideoContainer::Unknown)
             .value("MATROSKA", VideoContainer::Matroska)
             .value("AVI", VideoContainer::AVI)
             .export_values()
-            ;
+    ;
 
     py::enum_<DisplayMode>(m, "DisplayMode", py::arithmetic())
             .value("RAW_FRAMES", DisplayMode::RawFrames)
             .value("BACKGROUND_DIFF", DisplayMode::BackgroundDiff)
             .export_values()
-            ;
+    ;
+
+    py::enum_<ControlKind>(m, "ControlKind", py::arithmetic())
+            .value("UNKNOWN", ControlKind::Unknown)
+            .value("SELECTOR", ControlKind::Selector)
+            .value("SLIDER", ControlKind::Slider)
+            .export_values()
+    ;
+
+    py::class_<ControlDefinition>(m, "ControlDefinition")
+        .def(py::init<>())
+
+        .def_readwrite("kind", &ControlDefinition::kind, "Type of this control")
+        .def_readwrite("id", &ControlDefinition::id, "Identifier of this control")
+        .def_readwrite("name", &ControlDefinition::name, "Human-readable name for this control")
+
+        .def_readwrite("value_min", &ControlDefinition::valueMin, "Minimum possible value for this control")
+        .def_readwrite("value_max", &ControlDefinition::valueMax, "Maximum possible value for this control")
+        .def_readwrite("value_start", &ControlDefinition::valueStart, "Initial value for this control")
+        .def_readwrite("step_size", &ControlDefinition::stepSize, "Size of a single value step")
+
+        .def_readwrite("labels", &ControlDefinition::labels, "Labels for individual values (mostly used for ControlKind.SELECTOR types, their index can be set as control value)")
+        .def_readwrite("values", &ControlDefinition::values, "Possible values for this control")
+    ;
 
     py::class_<Miniscope>(m, "Miniscope")
         .def(py::init<>())
@@ -73,6 +103,10 @@ PYBIND11_MODULE(miniscope, m) {
         .def("start_recording", &Miniscope::startRecording, "Start recording a video file")
         .def("stop_recording", &Miniscope::stopRecording, "Finish the current recording")
 
+        .def_property_readonly("controls", &Miniscope::controls, "Get available controls for this device")
+        .def("control_value", &Miniscope::controlValue, "Retrieve current control value for the given control ID")
+        .def("set_control_value", &Miniscope::setControlValue, "Set new value for control with the given ID")
+
         .def("set_visible_channels", &Miniscope::setVisibleChannels, "Set which channels (red, green, blue) should be visible")
         .def_property_readonly("show_red_channels", &Miniscope::showRedChannel)
         .def_property_readonly("show_green_channels", &Miniscope::showGreenChannel)
@@ -85,7 +119,7 @@ PYBIND11_MODULE(miniscope, m) {
         .def_property_readonly("current_disp_frame", &Miniscope::currentDisplayFrame, "Retrieve the current frame intended for display. May not be the recorded frame.")
         .def_property_readonly("current_fps", &Miniscope::currentFps)
         .def_property_readonly("dropped_frames_count", &Miniscope::droppedFramesCount)
-        .def_property_readonly("last_recorded_frame_time", &Miniscope::lastRecordedFrameTime) // TODO: we need to add a converter for the return type
+        .def_property_readonly("last_recorded_frame_time", &Miniscope::lastRecordedFrameTime)
 
         .def_property("video_filename", &Miniscope::videoFilename, &Miniscope::setVideoFilename, "The name of the saved video")
         .def_property("video_codec", &Miniscope::videoCodec, &Miniscope::setVideoCodec, "The video codec to use")
@@ -102,6 +136,7 @@ PYBIND11_MODULE(miniscope, m) {
 
         .def_property("recording_slice_interval", &Miniscope::recordingSliceInterval, &Miniscope::setRecordingSliceInterval, "The interval at which new video files should be started when recording, in minutes")
 
+        .def("set_print_extra_debug", &Miniscope::setPrintExtraDebug, "Set whether protocol transmission debug messages should be printed to stdout")
         .def_property_readonly("last_error", &Miniscope::lastError, "Message of the last error, if there was one")
     ;
 }
