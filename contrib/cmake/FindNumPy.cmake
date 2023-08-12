02e86ec82439528b9f31cf622ae62d0b7b59304c
Matthias Klumpp <matthias@tenstral.net>
2023-08-12 02:19:30 +0200

    cmake: Stop using PythonInterp, use Python3 module instead in FindNumPy

--- contrib/cmake/FindNumPy.cmake
+++ contrib/cmake/FindNumPy.cmake
@@ -40,20 +40,19 @@
 
 # Finding NumPy involves calling the Python interpreter
 if(NumPy_FIND_REQUIRED)
-    find_package(PythonInterp REQUIRED)
+    find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 else()
-    find_package(PythonInterp)
+  find_package(Python3 COMPONENTS Interpreter Development)
 endif()
 
 if(NOT PYTHONINTERP_FOUND)
     set(NUMPY_FOUND FALSE)
-    return()
 endif()
 
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+execute_process(COMMAND "${Python3_EXECUTABLE}" "-c"
     "import numpy as n; print(n.__version__); print(n.get_include());"
     RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-    OUTPUT_VARIABLE _NUMPY_VALUES_OUTPUT
+    OUTPUT_VARIABLE _NUMPY_VALUES
     ERROR_VARIABLE _NUMPY_ERROR_VALUE
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
@@ -63,39 +62,31 @@ if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
             "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
     endif()
     set(NUMPY_FOUND FALSE)
-    return()
+else()
+    # Convert the process output into a list
+    string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
+    string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
+    list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
+    list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
+
+    # Make sure all directory separators are '/'
+    string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
+
+    # Get the major and minor version numbers
+    string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
+    list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
+    list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
+    list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
+    string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
+    math(EXPR NUMPY_VERSION_DECIMAL
+        "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+
+    find_package_message(NUMPY
+        "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
+        "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
+
+    set(NUMPY_FOUND TRUE)
+    if(NUMPY_FOUND)
+      set(Python3_NumPy_FOUND 1)
+    endif()
 endif()
-
-# Convert the process output into a list
-string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES_OUTPUT})
-string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
-# Just in case there is unexpected output from the Python command.
-list(GET _NUMPY_VALUES -2 NUMPY_VERSION)
-list(GET _NUMPY_VALUES -1 NUMPY_INCLUDE_DIRS)
-
-string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" _VER_CHECK "${NUMPY_VERSION}")
-if("${_VER_CHECK}" STREQUAL "")
-    # The output from Python was unexpected. Raise an error always
-    # here, because we found NumPy, but it appears to be corrupted somehow.
-    message(FATAL_ERROR
-        "Requested version and include path from NumPy, got instead:\n${_NUMPY_VALUES_OUTPUT}\n")
-    return()
-endif()
-
-# Make sure all directory separators are '/'
-string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
-
-# Get the major and minor version numbers
-string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
-list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
-list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
-list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
-string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
-math(EXPR NUMPY_VERSION_DECIMAL
-    "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
-
-find_package_message(NUMPY
-    "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-    "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
-
-set(NUMPY_FOUND TRUE)
