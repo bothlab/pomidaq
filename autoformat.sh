88a86d20cd58771e159f5ee6aa7213963d9d5402
Matthias Klumpp <matthias@tenstral.net>
2026-09-09 05:58:15 +0200

    trivial: Update autoformat helper

--- autoformat.sh
+++ autoformat.sh
@@ -3,6 +3,6 @@
 cd "$(dirname "$0")"
 set -e
 
-source_files=`find libminiscope/ src/ py/ -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.hpp' \)`
+source_files=`find miniscope/ src/ python/ -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.hpp' \)`
 
 clang-format -i $source_files
